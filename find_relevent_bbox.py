511375ccd686560f60c263a1120c55976d0f6d69
yhsben88 <yhsben@gmail.com>
2026-08-20 15:04:34 -0600

    feat: improved algorithm for finding monetary value. and improved documentation.

--- find_relevent_bbox.py
+++ find_relevent_bbox.py
@@ -13,18 +13,14 @@ DEV = False
 
 def find_relevent_bbox(ref_bbox, ref_text, reader_list) :
     '''
-    Scoring:
-        0: no relevent candidate could be found with the given reference box
-        1: candidate is vertically overlapping reference box to the left
-        2: candidate is overlapping reference box, vertically to the right | horizontally underneath
-        5: candidate sits below, near the center of reference box, is bigger than reference box
-        6: candidate sits below, near the center of reference box, shifted to the left
-        7: candidate sits below, near the center of reference box,shifted to the right
-        10: candidate sits to the right, near the center of reference box, candidate may be bigger | positioned higher than reference box
-        15: candidate is part of the reference bbox
-        16: candidate is exclusively to the right of reference box and sitting vertically higher than reference box
-        17: candidate sits to the right, near the center of reference box, shifted below the reference box
+    Regions of interest with respect to reference point are, columns & rows
+    c & r    0   1   2
+        0   00  01  02 
+        1   10  11  12
+        2   20  21  22
+        
     '''
+
     ref_x1, ref_y1, ref_x2, ref_y2 = bbox_bounds(ref_bbox)
     ref_x_center, ref_y_center = get_bbox_center(ref_bbox)
     top_score = 1
@@ -44,14 +40,17 @@ def find_relevent_bbox(ref_bbox, ref_text, reader_list) :
             score += 6
             if DEV: 
                 HAS_MONEY = True 
+                print(f"\t Is a monetary value")
         elif extract_money(text):
             score += 5 # specifically has to make the candidate worth more than position alone
             if DEV:
-                HAS_MONEY = True if DEV else False
+                HAS_MONEY = True
+                print(f"\t Is a possible monetary value")
 
 
-        # Reference bbox itself may contain the value
-        if point_is_inside_bbox(ref_x_center, ref_y_center, bbox):
+        # Reference bbox itself may contain the value, region [1][1]
+        is_inside_ref_bbox = point_is_inside_bbox(ref_x_center, ref_y_center, bbox)
+        if is_inside_ref_bbox:
             if (t := extract_money(text)) is not None:
 
                 position = money_relative_to_keyword(text, ref_text)
@@ -80,39 +79,37 @@ def find_relevent_bbox(ref_bbox, ref_text, reader_list) :
                 continue
             
         # Candidate is exclusively to the right of reference bbox
-        if x1 >= ref_x2:
-            if y1 <= ref_y_center <= y2:
+        if x1 >= ref_x2: # if region in column [2]
+            if y1 <= ref_y_center <= y2: # if region [1][2]
                 score += 16
                 if HAS_MONEY:
                     print(f"\tIs exclusively right & vertically centered")
-            elif ref_y1 < y2 and y1 < ref_y2:
+            elif ref_y1 < y2 and y1 < ref_y2: # if region between [1][2] & [2][2]
                 score += 7
                 if HAS_MONEY:
                     print(f"\tIs exclusively right & vertically close")
-        # Candidate exclusively vertically under reference bbox
-        if ref_y2 <= y1 and x1 <= ref_x_center <= x2:
+
+        # Candidate exclusively vertically under reference bbox, if region [2][1]
+        if ref_y2 <= y1 and x1 <= ref_x_center <= x2: 
             score += 6
             if HAS_MONEY:
                 print(f"\tIs exclusively below & horizontally centered")
 
-        # Candidate remotely overlaps vertically or horizontally under reference bbox, extra points for relevence
-        if (ref_y1 <= y1 <= ref_y2 and ref_y2 <= y2 and (ref_x1 <= x1 <= ref_x2 or ref_x1 <= x2 <= ref_x2)) or (ref_x1 <= x1 <= ref_x2 and ref_y1 <= y2):
+        # Candidate overlaps vertically or horizontally, and does not count as sitting inside reference box, 
+        # overlaps [1][1] but not counting as [1][1].
+        if not is_inside_ref_bbox and ref_y1 <= ( y1 or y2 ) <= ref_y2 and (ref_x1 <= ( x1 or x2 ) <= ref_x2):
             score += 3 
             if HAS_MONEY:
                 print(f"\tIs overlapping")
         
-        # Candidate overlaps and sits to the left of reference bbox
-        elif ref_x1 < x2 < ref_x2 and ref_y2 <= y2:
-            score += 3
-            if HAS_MONEY:
-                print(f"\tIs overlapping below & slightly left")
-        # if Candidate has no overlap 
+        # if Candidate has no overlap and sits in regions,
+        #  [0][0], [0][2], [2][0], [2],[2]
         elif ref_x2 < x1 or x2 < ref_x1:
-            if ref_y2 < y1:
+            if ref_y2 < y1: # region [2][0], [2],[2]
                 score += 3
                 if HAS_MONEY:
                     print(f"\t May be irrelavent, sits below reference")
-            elif y2 < ref_y1:
+            elif y2 < ref_y1: # region [0][0], [0][2]
                 score += 2
                 if HAS_MONEY:
                     print(f"\t May be irrelavent, sits above the reference.")
